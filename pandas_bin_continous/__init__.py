0aa3f71470af9fc3eccda8432bfc1bd608ae0994
jtloong <joshua.t.loong@gmail.com>
2018-12-03 17:58:14 -0500

    Changed new feature naming scheme

--- pandas_bin_continous/__init__.py
+++ pandas_bin_continous/__init__.py
@@ -3,8 +3,8 @@ import pandas as pd
 def create_features(df, bin_edges, feature_name):
 	bin_series = pd.cut(df[feature_name], bins=bin_edges)
 
-	for edge in bin_edges:
-		name = feature_name + '_' + str(edge) + '_to_' + str(edge + 1)
+	for index, edge in enumerate(bin_edges):
+		name = feature_name + '_' + str(edge) + '_to_' + str(bin_edges[index + 1])
 		df[name] = bin_series.apply(lambda i: 1 if i.left == edge else 0)
 
 	return df
