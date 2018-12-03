6504c9149ad6181d3a5ab89cd20c93ba56a93190
jtloong <joshua.t.loong@gmail.com>
2018-12-03 17:51:12 -0500

    Edited feature variable string

--- pandas_bin_continous/__init__.py
+++ pandas_bin_continous/__init__.py
@@ -1,10 +1,10 @@
 import pandas as pd 
 
 def create_features(df, bin_edges, feature_name):
-	bin_series = pd.cut(df['feature_name'], bins=bin_edges)
+	bin_series = pd.cut(df[feature_name], bins=bin_edges)
 
 	for edge in bin_edges:
-		name = feature_name + str(edge) + '_to_' _ str(edge + 1)
+		name = feature_name + '_' + str(edge) + '_to_' + str(edge + 1)
 		df[name] = bin_series.apply(lambda i: 1 if i.left == edge else 0)
 
 	return df
