65552435106bfc0be10eb761ace9dfdb301d7ffe
jtloong <joshua.t.loong@gmail.com>
2018-12-03 19:37:36 -0500

    Added docstring to the module

--- pandas_bin_continuous/__init__.py
+++ pandas_bin_continuous/__init__.py
@@ -1,6 +1,14 @@
 import pandas as pd 
 
 def create_features(df, bin_edges, feature_name):
+	"""Returns your Pandas dataframe with new binary features based on binned values of your selected feature.
+    Args:
+        df: A Pandas Dataframe that includes your continuous feature
+        bin_edges: A list of number by which to create the bounds of the bins
+        feature_name: A string that is the name of the feature we are binning
+    Returns:
+        df: A Pandas DataFrame that includes your original and your new binary encoded features
+    """
 	bin_series = pd.cut(df[feature_name], bins=bin_edges)
 
 	for index, edge in enumerate(bin_edges):
