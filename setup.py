4e6313a5c117b0eee3525027be33968affd2e552
jtloong <joshua.t.loong@gmail.com>
2018-12-03 19:30:11 -0500

    Fixed more spelling errors

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='pandas_bin_continous',
+setup(name='pandas_bin_continuous',
       version=' 1.0',
       description='Package to encode continous variables into binned features.',
       url='http://github.com/jtloong/pandas_bin_continous',
       author='Joshua Loong',
       author_email='joshua.t.loong@gmail.com',
       license='MIT',
-      packages=['pandas_bin_continous'],
+      packages=['pandas_bin_continuous'],
       install_requires=[
           'pandas'
       ],
